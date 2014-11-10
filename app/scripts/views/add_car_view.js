5577d4a35ffbab946f248a11aa0adcf39a3c1789
Thomas McCracken <thomas.alan.mccracken@gmail.com>
2014-11-09 19:51:10 -0500

    refactored and fixed bugs

--- app/scripts/views/add_car_view.js
+++ app/scripts/views/add_car_view.js
@@ -25,11 +25,12 @@
     addNewCar: function (e) {
       e.preventDefault();
 
-      //Grab feels values from my form
+      //store the car values
       var car_name = $('#name').val();
       var car_make = $('#make').val();
       var car_model = $('#model').val();
       var car_topSpeed = $('#topSpeed').val();
+      var car_description = $('#description').val();
       var car_image = $('#imgSrc').val();
 
       var car = new App.Models.Car({
@@ -37,6 +38,7 @@
         make: car_make,
         model: car_model,
         topSpeed: car_topSpeed,
+        description: car_description,
         imgSrc: car_image
       });
 
